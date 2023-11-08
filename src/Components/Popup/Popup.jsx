9ba85037a90e0d6d894dcede7369c677a7adc35a
Sumit Maurya <smithdashdash@gmail.com>
2023-11-08 18:31:12 +0530

    fixed design

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -25,26 +25,34 @@ const Popup = ({list,setList,setDisplayPopup}) => {
       setDisplayPopup(false);
     }
   return (
-    <div className={Styles.popup}>
-      <div className={Styles.heading}>Create New Notes Group</div>
-      <div className={Styles.name}>
-        <div className={Styles.subHeadingOne}>Group Name</div>
-        <input type='text' placeholder='Enter your group name...'
-        value={groupName} onChange={(e)=>setGroupName(e.target.value)}
-        />
-      </div>
-      <div className={Styles.choices}>
-        <div className={Styles.subHeadingTwo}>Choose Colour</div>
+    <div className={Styles.popup_container}>
+      <div className={Styles.popup}>
+        <div className={Styles.heading}>Create New Notes group</div>
+        <div className={Styles.name_container}>
+          <div className={Styles.subHeading}>Group Name </div>
+          <div className={Styles.name_input}>
+            <input type='text' placeholder='Enter your group name...'
+            value={groupName} onChange={(e)=>setGroupName(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <div className={Styles.choice_container}>
+          <div className={Styles.subHeading}>Choose colour</div>
+        <div className={Styles.container}>
         {COLOR_CHOICES.map((item,idx)=>
         <div className={Styles.colorDiv} 
         style={{backgroundColor:item.color,
         border: selectedColor === idx ? "4px solid black":"4px solid"+item.color}}
         key={idx} onClick={()=>setSelectedColor(idx)}/>
         )}
-      </div>
-      <button className={Styles.createBtn} onClick={handleClick}>
+        </div>
+        </div>
+        <div className={Styles.button}>
+        <button className={Styles.create_button} onClick={handleClick}>
         Create
       </button>
+        </div>
+      </div>
     </div>
   )
 }
